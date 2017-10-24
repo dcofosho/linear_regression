1925a0bf2b069d886562965c5e7bacae9c94a01d
Daniel Cohen <dlcohen224@gmail.com>
2017-10-24 17:35:01 -0400

    Feature: Added error handling when x_vals and y_vals are different lengths. Fixed readme

--- __init__.py
+++ __init__.py
@@ -27,45 +27,40 @@ from sklearn import linear_model
 import matplotlib.pyplot as plt
 app = Flask(__name__)
 
-
 @app.route('/linreg', methods=['GET','POST'])
 def linreg():
         if request.method=='POST':
-                #get x and y values from template
-                if request.form['x_vals']:
-                        x_vals=request.form['x_vals'].split(',')
-                if request.form['y_vals']:
-                        y_vals = request.form['y_vals'].split(',')
-                #determine which array is shorted, x or y. We will only use the number of data points in the smaller array
-                if len(x_vals)<len(y_vals):
-                        smaller_len = len(x_vals)
-                else:
-                        smaller_len = len(y_vals)
-                #convert array vals to int and remove u character
-                for i in range(0, smaller_len):
-                        x_vals[i] = [int(x_vals[i].replace('u',''))]
-                for i in range(0, smaller_len):
-                        y_vals[i] = [int(y_vals[i].replace('u',''))]
-                print(str(x_vals)+str(y_vals))
-                #plot the data using matplotlib
-                fig = plt.figure()
-                fig.suptitle('test title', fontsize=20)
-                plt.scatter(x_vals, y_vals)
-                #get regression line
-                reg = linear_model.LinearRegression()
-                reg.fit(x_vals, y_vals)
-                plt.plot(x_vals, reg.predict(x_vals), color='blue', linewidth=3)
-                slope = str(reg.coef_).replace('[','').replace(']', '')
-                intercept = str(reg.intercept_).replace('[','').replace(']', '')
-                r_squared = str(reg.score(x_vals, y_vals))
-                fig.suptitle('y='+slope+'x'+'+'+intercept+"          "+'r-squared='+r_squared, fontsize=20)
-                plt.xlabel="X axis"
-                plt.ylabel="Y axis"
-                out_png = '/var/www/html/spamfilter/spamfilter/out_file.png'
-                plt.savefig(out_png, dpi=150)
-                return send_file(out_png, mimetype='image/png')
+                try:
+                        if request.form['x_vals']:
+                                x_vals=request.form['x_vals'].split(',')
+                        if request.form['y_vals']:
+                                y_vals = request.form['y_vals'].split(',')
+                        #convert array vals to int and remove u character
+                        for i in range(0, len(x_vals)):
+                                x_vals[i] = [int(x_vals[i].replace('u',''))]
+                        for i in range(0, len(y_vals)):
+                                y_vals[i] = [int(y_vals[i].replace('u',''))]
+                        print(str(x_vals)+str(y_vals))
+                        fig = plt.figure()
+                        fig.suptitle('test title', fontsize=20)
+                        plt.scatter(x_vals, y_vals)
+                        reg = linear_model.LinearRegression()
+                        reg.fit(x_vals, y_vals)
+                        plt.plot(x_vals, reg.predict(x_vals), color='blue', linewidth=3)
+                        slope = str(reg.coef_).replace('[','').replace(']', '')
+                        intercept = str(reg.intercept_).replace('[','').replace(']', '')
+                        r_squared = str(reg.score(x_vals, y_vals))
+                        fig.suptitle('y='+slope+'x'+'+'+intercept+"          "+'r-squared='+r_squared, fontsize=20)
+                        plt.xlabel="X axis"
+                        plt.ylabel="Y axis"
+                        out_png = '/var/www/html/spamfilter/spamfilter/out_file.png'
+                        plt.savefig(out_png, dpi=150)
+                        return send_file(out_png, mimetype='image/png')
+                except:
+                        return "X and Y need to be same length!"+"<br>"+"length of x_vals: "+str(len(x_vals))+"<br>"+"<br>"+"length of y_vals: "+str(len(y_vals))+"<br>"+"<button onclick='history.back()'>Go back</button>"
         else:
                 return render_template("linreg.html")
+
 
 
 if __name__ == '__main__':
